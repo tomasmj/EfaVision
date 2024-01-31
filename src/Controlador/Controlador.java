ad545a9d40a3d2a71dc17005e0ce717809ad8f7a
adridimo <adridimo04@gmail.com>
2024-01-31 10:42:46 +0100

    sfsfs

--- src/Controlador/Controlador.java
+++ src/Controlador/Controlador.java
@@ -1,5 +1,20 @@
 package Controlador;
 
-public class Controlador {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.text.View;
+
+public class Controlador implements ActionListener {
+
+	public Controlador(View vista) {
+		
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
