530ce4d64a95a5db8ae834c02f66acc487f1114b
Tomas Merino <tomasmerino2003@gmail.com>
2024-01-31 17:13:46 +0100

    fdf

--- src/Controlador/Controlador.java
+++ src/Controlador/Controlador.java
@@ -4,8 +4,10 @@ import java.awt.event.ActionListener;
 
 import javax.swing.text.View;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 import Vista.*;
 import Persistencias.*;
@@ -17,31 +19,38 @@ public class Controlador implements ActionListener {
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		SessionFactory sesion = null;
-
 		if(vista.Simular == e.getSource()) {
-			
+			SessionFactory sesion = null;
 			try {
 				Configuration configuracion = new Configuration();
 				configuracion.configure("hibernate.cfg.xml");
 				sesion = configuracion.buildSessionFactory();
 				System.out.println("Conexion a la base de datos realizada");
+				
+				eliminarMenores(sesion);
+				System.out.println("Eliminar columnas");
+				
+				
 			} catch(Exception a) {
 				a.printStackTrace();
 				throw a;
 			}
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
+		}
+	}
+	
+	public void eliminarMenores(SessionFactory sesion) {
+		Session session;
+		try {
+			session = sesion.getCurrentSession();
+			session.beginTransaction();
+
+			Query query = session.createSQLQuery("ALTER TABLE porcentajes_rangoedad DROP COLUMN rango_1_9, DROP COLUMN rango_10_17");
+			query.executeUpdate();
+
+			session.getTransaction().commit();
+
+		}catch(Exception e) {
+			e.printStackTrace();
 		}
 	}
 
